e73338a564a6b7a7776426f16f0e1d5b2f94ae5f
rahulbhagotia97 <rahulbhagotia97@gmail.com>
2017-07-11 16:59:47 +0530

    Add files via upload
    added like list and comment list and introduced trending post

--- instabot.py
+++ instabot.py
@@ -2,7 +2,7 @@ import requests , urllib
 
 APP_ACCESS_TOKEN = '3077722633.9208c67.3ee4985723f146bfa542d6872c7fdf8e'
 # Token owner : bhagotiarahul
-# Sandbox users : instabot , mehulbiswas
+# Sandbox users : Vivek3273 , mehulbiswas , Streethustler_1
 
 BASE_URL = 'https://api.instagram.com/v1/'
 
@@ -74,10 +74,10 @@ def get_own_post():
 
     if own_media['meta']['code'] == 200:
         if len(own_media['data']):
-            image_name = own_media['data'][0]['id'] + '.jpeg'
-            image_url = own_media['data'][0]['images']['standard_resolution']['url']
-            urllib.urlretrieve(image_url, image_name)
-            print 'image has been downloaded'
+            video_name = own_media['data'][0]['id'] + '.mp4'
+            video_url = own_media['data'][0]['videos']['standard_resolution']['url']
+            urllib.urlretrieve(video_url, video_name)
+            print 'video has been downloaded'
         else:
             print 'Post does not exist!'
     else:
@@ -96,14 +96,19 @@ def get_user_post(insta_username):
 
     if user_media['meta']['code'] == 200:
         if len(user_media['data']):
-            image_name = user_media['data'][0]['id'] + '.jpeg'
-            image_url = user_media['data'][0]['images']['standard_resolution']['url']
-            urllib.urlretrieve(image_url,image_name)
-            print 'Your image has been downloaded!'
+            video_name = user_media['data'][0]['id'] + '.mp4'
+            video_url = user_media['data'][0]['videos']['standard_resolution']['url']
+            urllib.urlretrieve(video_url,video_name)
+            print 'Your video has been downloaded!'
+        elif len(user_media['data']):
+                image_name = user_media['data'][0]['id'] + '.jpeg'
+                image_url = user_media['data'][0]['images']['standard_resolution']['url']
+                urllib.urlretrieve(image_url, image_name)
+                print 'Your image has been downloaded!'
         else:
-            print 'Post does not exist!'
+            print 'post doesnot exist'
     else:
-        print 'Status code other than 200 received!'
+            print 'Status code other than 200 received!'
 
 # function declaration to get ID of recent post of a user
 
@@ -151,9 +156,69 @@ def post_a_comment(insta_username):
     make_comment = requests.post(request_url, payload).json()
 
     if make_comment['meta']['code'] == 200:
-        print "your comement was added"
+        print "your comment was added"
     else:
-        print "No comments could be added. Try again!"
+        print "No comments could be added , please try again."
+
+# function declaration of list of likes made by user on post
+
+def get_like_list(insta_username):
+    media_id=get_post_id(insta_username)
+    request_url = (BASE_URL + 'media/%s/likes') % (media_id)
+    print 'GET request url : %s' % (request_url)
+    like_list = requests.post(request_url).json()
+
+    if like_list['meta']['code'] == 200:
+        print like_list
+    else:
+        print 'status code other than 200 received'
+
+# function declaration of list of comments made on users post
+
+def get_comment_list(media_id):
+    list_of_comments = get_comment_list(media_id)
+    if list_of_comments== None:
+        print 'no media exists'
+        exit()
+    request_url = (BASE_URL + 'users/%s/media/recent/?access_token=%s') % (list_of_comments, APP_ACCESS_TOKEN)
+    print 'GET request url : %s' % (request_url)
+    comment_list = requests.get(request_url).json()
+
+    if comment_list['meta']['code'] == 200:
+        if len(comment_list['data']):
+            print 'list of comments : %s'%(comment_list['data'][0]['id'])
+        else:
+            print 'post has no comments '
+    else:
+        print 'status code other than 200 received'
+
+    if comment_list['meta']['code'] == 200:
+        print comment_list
+    else:
+        print 'status code other than 200 received'
+
+# function declaration for trending post
+
+def get_trending post(media_id):
+    list_of_comments = get_comment_list(media_id)
+    list_of_likes = get_like_list(media_id)
+    if list_of_likes== None:
+        print 'no trending post found'
+        exit()
+    request_url = (BASE_URL + 'users/%s/media/recent/?access_token=%s') % (list_of_likes,APP_ACCESS_TOKEN)
+    print 'GET request url : &s' % (request_url)
+    like_list = requests.get(request_url).json()
+    request_url = (BASE_URL + 'users/%s/media/recent/?access_token=%s') % (list_of_comments, APP_ACCESS_TOKEN)
+    print 'GET request url : &s' % (request_url)
+    comment_list = requests.get(request_url).json()
+
+    if comment_list['meta']['code'] == 200:
+    if like_list['meta']['code'] == 200:
+    if len((like_list['data'])>(comment_list['data'])):
+        print 'this post is trending : %s'% (user_info['data'][0]['id'])
+    else:
+        print'status code other than 200 received'
+
 
 # menu options of bot
 
@@ -199,4 +264,9 @@ def start_bot():
         exit()
     else:
      print "choice doesnt exist"
+
 start_bot()
+
+
+
+
